062c9e052b5b2390130e47004e67503e9575ba91
Brian Wright <bwright@webstaurantstore.com>
2017-10-04 21:30:46 -0400

    Initial commit - A photo gallery app using Angular JS and Firebase. Originally a tutorial by Wes Doyle.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+   apiKey: "AIzaSyByffQeCmVcncvk2kPr3jwpQKgEU9vkm9k",
+   authDomain: "angular-gallery-55db0.firebaseapp.com",
+   databaseURL: "https://angular-gallery-55db0.firebaseio.com",
+   projectId: "angular-gallery-55db0",
+   storageBucket: "angular-gallery-55db0.appspot.com",
+   messagingSenderId: "1077746229987"
+ }
+};
